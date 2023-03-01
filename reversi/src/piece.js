d5b77faeb8cba5ce3bcf7da6841491ed55a5c702
Alex L <luckyleck29@gmail.com>
2023-03-01 14:14:13 -0500

    Abort Controller

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -2,29 +2,39 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
-}
+
+    this.color = color;
+
+};
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+
+    return this.color === 'black' ? 'white' : 'black';
+
 };
+
+// black = new Piece('black');
+// console.log(black.oppColor())
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    
 };
 
 /**
  * Returns a string representation of the piece
  * based on its color.
  */
-Piece.prototype.toString = function () {
-};
+// Piece.prototype.toString = function () {
+// };
 
-// DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
-}
-// DON'T TOUCH THIS CODE
+// // DON'T TOUCH THIS CODE
+// if (typeof window === 'undefined'){
+//     module.exports = Piece;
+// }
+// // DON'T TOUCH THIS CODE
